cd4bf84a6685a730736f61549d03162ebc3adc09
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-17 23:11:16 -0400

    greetings function now has random capability

---
+++ greetings.go
@@ -0,0 +1,28 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
